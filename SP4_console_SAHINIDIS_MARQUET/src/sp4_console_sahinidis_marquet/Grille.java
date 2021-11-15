81a6d4bfc026615fb87d0e79cf6d4b2feb7dd386
matth <matth@192.168.115.197>
2021-11-15 11:53:27 +0100

    avancement

--- SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/Grille.java
+++ SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/Grille.java
@@ -9,13 +9,57 @@ package sp4_console_sahinidis_marquet;
  * @author matth
  */
 public class Grille {
-    Cellule [] CellulesJeu = new Cellule [6][7];
+    Cellule[][] CellulesJeu = new Cellule [6][7];
     public Grille(){
         for(int i=0; i<=7; i++ ){
             for (int j=0; j<=6; j++){
-                CellulesJeu[j][i] = new Cellule;
+                CellulesJeu[j][i] = new Cellule();
             }
         }
+    }
+    public void ajouterJetonDansColonne(Jeton jet, int c){
+        int a =0;
+        if(CellulesJeu[0][c]==null){
+            for(int i=0; i<=6; i++){
+                if(CellulesJeu[i][c]==null){
+                    a = i;
+                }
+            }
+            CellulesJeu[a][c].affecterJeton(jet);
+        }
+        else{
+            System.out.println("Cette colonne est pleine");
+        }
+    }
+    public boolean etreRemplie(){
+        int a=0;
+        for(int i=0; i<=7; i++ ){
+            for (int j=0; j<=6; j++){
+                if (CellulesJeu[j][i]==null){
+                    a+=1;
+                }
+            }
+        } 
+        if (a==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void viderGrille(){
+        for(int i=0; i<=7; i++ ){
+            for (int j=0; j<=6; j++){
+                CellulesJeu[j][i] = null;
+            }
+        }  
+    }
+    public void afficherGrilleSurConsole(){
+        for(int i=0; i<=7; i++ ){
+            for (int j=0; j<=6; j++){
+                System.out.print(CellulesJeu[j][i]);
+            }
+        }  System.out.print("\n");
     }
 }
 
