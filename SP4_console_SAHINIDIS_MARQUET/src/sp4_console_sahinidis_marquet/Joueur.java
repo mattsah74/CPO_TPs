f3ff93da6a4558e0f7836c0a5ac70abd5aac1d52
matth <matth@192.168.115.197>
2021-11-15 10:06:25 +0100

    avancement puissance 4

---
+++ SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/Joueur.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_sahinidis_marquet;
+import java.util.Random;
+
+/**
+ *
+ * @author matth
+ */
+public class Joueur {
+    String nom;
+    String couleur;
+    int ListeJetons;
+    int nombreJetonsRestants;
+    int nombreDesintegrateurs;
+   // String [] couleur = {"jaune", "rouge"};
+    public Joueur(String lenom, String couleur1, int listejet, int nbjetrest, int nbdesint){
+        nom = lenom;
+        couleur = couleur1;
+        ListeJetons = listejet;
+        nombreJetonsRestants = nbjetrest;
+        nombreDesintegrateurs = nbdesint;
+    }
+    public void affecterCouleur(String couleur1){
+        couleur = couleur1;
+    }
+}
