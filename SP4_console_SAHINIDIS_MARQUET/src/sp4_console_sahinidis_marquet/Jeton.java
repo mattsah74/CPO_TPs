fd461d65985a369143055f19d46780cec585fde4
matth <matth@192.168.115.197>
2021-11-15 10:25:25 +0100

    ajout de la fonction jeton

---
+++ SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/Jeton.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_sahinidis_marquet;
+
+/**
+ *
+ * @author matth
+ */
+public class Jeton {
+    String Couleur;
+    public Jeton(String CouleurJeton){
+        Couleur=CouleurJeton;
+    }
+    public String lireCouleur(){
+        return Couleur;
+    }
+}
+
