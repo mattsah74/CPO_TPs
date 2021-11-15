852ac960a2a8f26ab7ec436555986fad5e9db189
matth <matth@192.168.115.197>
2021-11-15 11:53:42 +0100

    avancement

--- SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/SP4_console_SAHINIDIS_MARQUET.java
+++ SP4_console_SAHINIDIS_MARQUET/src/sp4_console_sahinidis_marquet/SP4_console_SAHINIDIS_MARQUET.java
@@ -14,7 +14,9 @@ public class SP4_console_SAHINIDIS_MARQUET {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Grille tab = new Grille();
+        tab.afficherGrilleSurConsole();
+        
     }
     
 }
